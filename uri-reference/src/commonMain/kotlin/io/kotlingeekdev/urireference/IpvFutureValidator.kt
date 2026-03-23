b6989df629951a3fd62e53da7d77129d8417d3e3
KotlinGeekDev <kotlingeek@protonmail.com>
2026-03-23 17:27:15 +0100

    Update README. Fix some bugs.

--- uri-reference/src/commonMain/kotlin/io/kotlingeekdev/urireference/IpvFutureValidator.kt
+++ uri-reference/src/commonMain/kotlin/io/kotlingeekdev/urireference/IpvFutureValidator.kt
@@ -13,9 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package urireference
-
-import io.kotlingeekdev.urireference.Utils
+package io.kotlingeekdev.urireference
 
 
 /**
--- uri-reference/src/commonTest/kotlin/io/kotlingeekdev/urireference/IpvFutureValidatorTest.kt
+++ uri-reference/src/commonTest/kotlin/io/kotlingeekdev/urireference/IpvFutureValidatorTest.kt
@@ -15,7 +15,6 @@
  */
 package io.kotlingeekdev.urireference
 
-import urireference.IpvFutureValidator
 import kotlin.test.Test
 
 
