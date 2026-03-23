9adbb56302a0d80212441d599117d7f826c4dcdd
KotlinGeekDev <kotlingeek@protonmail.com>
2026-03-23 16:21:04 +0100

    Raise Android minSdk for now. Make sure library publication works correctly.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,64 @@
-
 plugins {
-    alias(libs.plugins.vanniktech.mavenPublish)
+    alias(libs.plugins.vanniktech.mavenPublish) version libs.versions.mavenPublish
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.androidKotlinMultiplatformLibrary) apply false
     alias(libs.plugins.android.lint) apply false
 }
 
-group = "io.github.kotlingeekdev"
-version = "1.0"
-description = "Kotlin Multiplatform URI Library Compliant with RFC 3986"
+allprojects {
+    apply(plugin = "com.vanniktech.maven.publish")
 
-val isJitpack = System.getenv("JITPACK") == "true"
+    group = "io.github.kotlingeekdev"
+    version = "1.0"
+    description = "Kotlin Multiplatform URI Library Compliant with RFC 3986"
 
-mavenPublishing {
-    publishToMavenCentral(automaticRelease = true)
-    if (!isJitpack) {
-        signAllPublications()
-    }
+    val isJitpack = System.getenv("JITPACK") == "true"
 
-    coordinates(group.toString(), rootProject.name, version.toString())
-
-    pom {
-        name = "Uri-reference-kmp"
-        description = "Kotlin Multiplatform URI Library Compliant with RFC 3986, based on the original Java library."
-        url = "https://github.com/KotlinGeekDev/uri-reference-kmp"
-
-        licenses {
-            license {
-                name = "Apache License, Version 2.0"
-                url = "https://opensource.org/license/apache-2.0"
-                distribution = "https://opensource.org/license/apache-2.0"
-            }
+    mavenPublishing {
+//    configure(
+//        KotlinMultiplatform(
+//            sourcesJar = SourcesJar.Sources(),
+//            androidVariantsToPublish = listOf("release"),
+//        )
+//    )
+        publishToMavenCentral(automaticRelease = true)
+        if (!isJitpack) {
+            signAllPublications()
         }
 
-        developers {
-            developer {
-                name = "KotlinGeekDev"
-                email = "kotlingeek@protonmail.com"
-                url = "https://github.com/KotlinGeekDev"
-            }
-            developer {
-                name = "Hideki Ikeda"
-                email = "hidebike712@gmail.com"
-                url = "https://github.com/hidebike712"
-            }
-        }
+        coordinates(group.toString(), rootProject.name, version.toString())
 
-        scm {
-            connection = "scm:git:git://github.com/KotlinGeekDev/uri-reference-kmp.git"
+        pom {
+            name = "Uri-reference-kmp"
+            description = "Kotlin Multiplatform URI Library Compliant with RFC 3986, based on the original Java library."
             url = "https://github.com/KotlinGeekDev/uri-reference-kmp"
 
+            licenses {
+                license {
+                    name = "Apache License, Version 2.0"
+                    url = "https://opensource.org/license/apache-2.0"
+                    distribution = "https://opensource.org/license/apache-2.0"
+                }
+            }
+
+            developers {
+                developer {
+                    name = "KotlinGeekDev"
+                    email = "kotlingeek@protonmail.com"
+                    url = "https://github.com/KotlinGeekDev"
+                }
+                developer {
+                    name = "Hideki Ikeda"
+                    email = "hidebike712@gmail.com"
+                    url = "https://github.com/hidebike712"
+                }
+            }
+
+            scm {
+                connection = "scm:git:git://github.com/KotlinGeekDev/uri-reference-kmp.git"
+                url = "https://github.com/KotlinGeekDev/uri-reference-kmp"
+
+            }
         }
     }
 }
