c05fa1441c0ae272778ba241417de9914021556f
KotlinGeekDev <kotlingeek@protonmail.com>
2026-03-23 11:42:35 +0100

    Move code to inner uri-reference module to support Android library builds. Modify publishing information, as well as structure of build files.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,17 @@
 pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 rootProject.name = "uri-reference-kmp"
+include(":uri-reference")
